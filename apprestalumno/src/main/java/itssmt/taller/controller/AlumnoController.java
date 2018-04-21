955ed1b77bf35a2cbcd5ade6b00e3bd9493a7a4a
Tirso Reyes <tirso.reyes.hdez@gmail.com>
2018-04-21 12:11:51 -0500

    post y get de ejemplos

--- apprestalumno/src/main/java/itssmt/taller/controller/AlumnoController.java
+++ apprestalumno/src/main/java/itssmt/taller/controller/AlumnoController.java
@@ -1,8 +1,14 @@
 package itssmt.taller.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import itssmt.taller.modelo.Alumno;
+import itssmt.taller.modelo.GenericResponse;
 
 @RestController
 @RequestMapping("/rest")
@@ -12,6 +18,42 @@ public class AlumnoController {
 	public String test(){
 		return "hello world!";
 	}
+	
+	
+	@GetMapping(value="/respuesta")
+	public String respuesta(@RequestParam(value="nombre",required=true) String tuNombre){
+		
+		return "hello  "+tuNombre;
+	}
+	
+	
+	
+	@GetMapping(value="/respuesta2")
+	public String respuesta2sdfsdf(@RequestParam(value="nombre",required=true) String tuNombre,
+			@RequestParam(value="apellido",required=false) String tuApellido){
+		
+		String apellido=  tuApellido==null?"sin apellido":tuApellido;
+		
+		return "hello  "+tuNombre+" "+apellido;
+	}
+	
+	
+	@PostMapping(value="/saveAlumno")
+	public GenericResponse<Alumno> saveAlumno(@RequestBody Alumno alumno){
+		GenericResponse<Alumno> response = new GenericResponse<>();
+		
+		
+		response.setCode(200);
+		response.setMessage("El alumno "+alumno.getNombre()+ " se guardo correctamente.");
+		response.setResponse(alumno);
+		
+		return response;
+		
+	}
+	
+	
+	
+	
 	
 	
 }
---
+++ apprestalumno/src/main/java/itssmt/taller/modelo/Alumno.java
@@ -0,0 +1,35 @@
+package itssmt.taller.modelo;
+
+public class Alumno {
+	
+	
+	private String nombre;
+	private String apellidos;
+	private int idMaestro;
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public int getIdMaestro() {
+		return idMaestro;
+	}
+	public void setIdMaestro(int idMaestro) {
+		this.idMaestro = idMaestro;
+	}
+	
+	
+	
+	
+
+}
